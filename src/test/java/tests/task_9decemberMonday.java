0063bbe0e4421f4020a25a535a26d92f1cf8c46b
sibelBel <57195562+sibelBel@users.noreply.github.com>
2019-12-09 18:37:44 +0100

    Update task_9decemberMonday.java

--- src/test/java/tests/task_9decemberMonday.java
+++ src/test/java/tests/task_9decemberMonday.java
@@ -2,5 +2,11 @@ package tests;
 
 public class task_9decemberMonday {
 
+  public static void main(String [] args){
+  
+    System.out.println("Hello world");
+    //this line is adding from gitHub, see you in IntelliJ
+  
+  }
 
 }
