4b75fab9d02edc49b528c76e3f2f21c9253ae931
sibelBel <57195562+sibelBel@users.noreply.github.com>
2019-12-16 18:47:32 +0100

    bir şeyler ekle

--- src/test/java/tests/poll.java
+++ src/test/java/tests/poll.java
@@ -5,7 +5,7 @@ import org.w3c.dom.ls.LSOutput;
 public class poll {
 
     public static void main(String[] args) {
-
+        //bir şeyler eklememiz gerekiyor galııbaaa:))
         System.out.println("helloooo");
     }
 }
